24296f25b294a4c146897b897b7d7367ee2c7de4
thejordanprice <thejordanprice@gmx.com>
2018-02-26 08:23:54 +0000

    added error checking and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gencerts.sh
 An easy to use bash script I've create for implementing Lets Encrypt to many systems. As if it wasn't already easy enough.
+
+### Usage
+
+1. Install [letsencrypt](https://letsencrypt.org) to your machine.
+2. Git clone this repository.
+3. cd to gencerts/.
+4. Edit the domain names in the config inside gencerts.sh.
+5. The domain names listen in the config must have an A record pointing to that box.
+3. Run ./gencerts.sh
+
+You shouldn't have many issues :smile:
--- gencerts.sh
+++ gencerts.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-sudo certbot certonly --standalone \
- -d 'example.com' \
- -d 'www.example.com' \
- -d 'www2.example.com' \
- -d 'someother.sub.com'
+function checkCertBot() {
+  # check that certbot is installed
+  if hash certbot 2>/dev/null;
+    then
+      printf "certbot has been located\n";
+      sudo certbot certonly --standalone \
+        -d 'example.com' \
+        -d 'www.example.com' \
+        -d 'www2.example.com' \
+        -d 'someother.sub.com'
+    else
+      printf "goto https://certbot.eff.org\n"
+      exit
+  fi
+}
+checkCertBot
